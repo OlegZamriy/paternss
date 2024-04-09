9e1262f340021680b1abbbe31001c77773b7a57f
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 18:47:19 +0300

    gdrgdg

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using paternss;
+using System;
+
+class Program
+{      
+    static void Main()
+    {
+        Singleton singletonInstance = Singleton.Instance;
+        singletonInstance.SomeMethod();
+
+
+        Console.ReadLine();
+    }
+}
---
+++ Singleton .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paternss
+{
+    public class Singleton : ISingleton
+    { 
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        public void SomeMethod()
+        {
+            Console.WriteLine("Вызван метод из Singleton");
+        }
+    }
+
+}
