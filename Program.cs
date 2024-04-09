68d7fa6f899fb685696efed5189e49106a1ef2c8
Олег Замрій <mynameishelg@gmail.com>
2024-04-09 18:57:17 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using paternss;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Sheep originalSheep = new Sheep("Dolly", 2);
+        originalSheep.DisplayInfo();
+
+        Sheep clonedSheep = (Sheep)originalSheep.Clone();
+        clonedSheep.Name = "Molly";
+        clonedSheep.Age = 3;
+        clonedSheep.DisplayInfo();
+
+        Console.ReadLine();
+    }
+}
---
+++ Prototype.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace paternss
+{
+     using System;
+
+    public class Sheep : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Sheep(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public ICloneable Clone()
+        {
+             return new Sheep(Name, Age);
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Sheep: {Name}, Age: {Age}");
+        }
+    }
+
+}
